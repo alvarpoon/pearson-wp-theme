8406221604808377c161f4c41d448a9962eece18
ronksc <ksc.ron@gmail.com>
2018-05-10 23:32:16 +0800

    Initial commit

---
+++ wp-content/themes/master/templates/footer.php
@@ -0,0 +1,17 @@
+<footer class="content-info" role="contentinfo">
+  <div class="container">
+    <?php //dynamic_sidebar('sidebar-footer'); ?>
+	<div class="footer-wrapper" style="">
+		<div class="footer-links">
+			<a href="#">Terms of Use</a>
+			<a href="#">Privacy Policy</a>
+			<a href="#">Accessibility</a>
+		</div>
+		<div class="footer-copyright">
+			Copyright &copy; 2018 Pearson Education Inc. All Rights Reserved.
+		</div>
+	</div>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
---
+++ wp-content/themes/master/templates/header-top-navbar.php
@@ -0,0 +1,40 @@
+<header class="banner navbar navbar-default" role="banner">
+	<div class="topbar-wrapper">
+	  <div class="container">
+		<div class="clearfix">
+			<div class="navbar-header">
+			  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+			  <a href="javascript:;" class="menu-label hidden-xs hidden-sm hidden-md hidden-lg" data-toggle="collapse" data-target=".navbar-collapse">menu</a>
+			  <a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/common/pearson-logo@2x.png" class="img-responsive"></a>
+			</div>
+			<div class="navbar-header-right hidden-xs hidden-sm visible-md visible-lg">
+				<div class="lang-wrapper">
+					<a href="#">中文</a>
+					<a href="#">English</a>			
+				</div>
+				<div class="login-wrapper">
+					<a href="#">Sign in</a>
+				</div>
+			</div>
+		</div>
+	  </div>
+	</div>
+  <div class="nav-container">
+	<div class="container">
+		<nav class="collapse navbar-collapse main-menu" role="navigation">
+			<?php
+				//Main menu
+				if (has_nav_menu('primary_navigation')) :
+				  wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav', 'depth' => 3));
+				endif;
+	
+			?>
+		</nav>
+	</div>
+  </div>
+</header>
