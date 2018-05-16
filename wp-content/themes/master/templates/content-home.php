ed4a47bcf71c61c1cda813d1707282a6fee721ab
ronksc <ksc.ron@gmail.com>
2018-05-16 22:03:40 +0800

    home update

--- wp-content/themes/master/templates/content-home.php
+++ wp-content/themes/master/templates/content-home.php
@@ -1,32 +1,24 @@
 <?php //the_content(); ?>
 <?php //wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 
-<div class="top-banner-container">
-	<img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/banner-top.png" class="img-responsive" />
+<div class="top-banner-container" style="background-image:url('<?=get_stylesheet_directory_uri()?>/assets/img/home/banner-top.png');">
+	<img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/banner-top.png" class="img-responsive hidden-xs hidden-sm visible-md visible-lg" />
 </div>
 
 <div class="home-slider">
-	<div class="slider-item">
-		<img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/slider-banner-1.png" class="img-responsive" />
+	<div class="slider-item" style="background-image:url('<?=get_stylesheet_directory_uri()?>/assets/img/home/slider-banner-1.png');">
+		<img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/slider-banner-1.png" class="img-responsive hidden-xs hidden-sm hidden-md visible-lg" />
 	</div>
-	<div class="slider-item">
-		<img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/slider-banner-1.png" class="img-responsive" />
+	<div class="slider-item" style="background-image:url('<?=get_stylesheet_directory_uri()?>/assets/img/home/slider-banner-1.png');">
+		<img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/slider-banner-1.png" class="img-responsive hidden-xs hidden-sm hidden-md visible-lg" />
 	</div>
-	<div class="slider-item">
-		<img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/slider-banner-1.png" class="img-responsive" />
+	<div class="slider-item" style="background-image:url('<?=get_stylesheet_directory_uri()?>/assets/img/home/slider-banner-1.png');">
+		<img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/slider-banner-1.png" class="img-responsive hidden-xs hidden-sm hidden-md visible-lg" />
 	</div>
 </div>
 
 <div class="home-content-wrapper">
 	<div class="container">
-		<div class="content-left">
-			<div class="banner-item"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/side-banner-1.png" class="img-responsive" /></a></div>
-			<div class="banner-item"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/side-banner-2.png" class="img-responsive" /></a></div>
-			<div class="banner-item"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/side-banner-3.png" class="img-responsive" /></a></div>
-			<div class="banner-item"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/side-banner-4.png" class="img-responsive" /></a></div>
-			<div class="banner-item"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/side-banner-5.png" class="img-responsive" /></a></div>
-			<div class="banner-item"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/side-banner-6.png" class="img-responsive" /></a></div>
-		</div>
 		<div class="content-right col-md-12">
 			<div class="section-title">What's New</div>
 			<div class="scrollable-content-wrapper">
@@ -100,5 +92,14 @@
 				</div>
 			</div>
 		</div>
+		<div class="content-left">
+			<div class="banner-item"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/side-banner-1.png" class="img-responsive" /></a></div>
+			<div class="banner-item"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/side-banner-2.png" class="img-responsive" /></a></div>
+			<div class="banner-item"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/side-banner-3.png" class="img-responsive" /></a></div>
+			<div class="banner-item"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/side-banner-4.png" class="img-responsive" /></a></div>
+			<div class="banner-item"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/side-banner-5.png" class="img-responsive" /></a></div>
+			<div class="banner-item"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/side-banner-6.png" class="img-responsive" /></a></div>
+		</div>
+		
 	</div>
 </div>
