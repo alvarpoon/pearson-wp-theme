e6806aaf11f790999bc1358ac1362ed966582e5c
ronksc <ksc.ron@gmail.com>
2018-07-08 16:27:33 +0800

    UI bug fix

--- wp-content/themes/master/lib/post-types.php
+++ wp-content/themes/master/lib/post-types.php
@@ -94,4 +94,35 @@ function service_code_register() {
   );
   register_post_type( 'service code' , $args );
 }
+
+// Home banner
+add_action('init', 'home_banner_register');
+function home_banner_register() {
+  $labels = array(
+      'name' => _x('Mainpage banner', 'post type general name'),
+      'singular_name' => _x('Mainpage banner', 'post type singular name'),
+      'add_new' => _x('Add Mainpage banner', 'rep'),
+      'add_new_item' => __('Add New Mainpage banner'),
+      'edit_item' => __('Edit Mainpage banner'),
+      'new_item' => __('New Mainpage banner'),
+      'view_item' => __('View Mainpage banner'),
+      'search_items' => __('Search Mainpage banner'),
+      'not_found' =>  __('Nothing found'),
+      'not_found_in_trash' => __('Nothing found in Trash'),
+      'parent_item_colon' => ''
+  );
+  $args = array(
+      'labels' => $labels,
+      'public' => true,
+      'publicly_queryable' => true,
+      'show_ui' => true,
+      'query_var' => true,
+      'rewrite' => true,
+      'capability_type' => 'post',
+      'hierarchical' => true,
+      'menu_position' => 8,
+      'supports'      => array( 'title', 'editor', 'revisions'),
+  );
+  register_post_type( 'mainpage banner' , $args );
+}
 ?>
